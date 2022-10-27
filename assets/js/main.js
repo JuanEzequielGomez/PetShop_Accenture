267b05799f2efed8c24b1be5bc9037d9a59cd55c
kalanchoe18 <marcoaleman18@gmail.com>
2022-10-27 13:13:43 -0300

    Solo corrijo cosas

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,21 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            articulos: [],
-            backUpArticulos: [],
             urlApi: "https://apipetshop.herokuapp.com/api/articulos",
-            categorias: [],
+            backUpArticulos: [],
+            articulos: [],
             articulosFarmacia: [],
             articulosJugueteria: [],
-
+            categorias: [],
+            detalleArt: [],
+            carrito: []
         }
     },
     created() {
-        this.traerDatos()
+        this.traerDatos();
+        if(localStorage.getItem('carrito')){
+            this.carrito = JSON.parse(localStorage.getItem('carrito'));
+        }
     },
     mounted() {
 
@@ -29,8 +33,8 @@ createApp({
                         this.articulos = data.response.filter((articulo) => articulo.tipo == "Juguete")
                     } else if (document.title == "PLEM Detalle") {
                         let id = new URLSearchParams(location.search).get("_id");
-                        this.articulos = data.response.find((articulo) => articulo._id == id);
-                        console.log(articulos)
+                        this.detalleArt = data.response.find((articulo) => articulo._id == id);
+
                     }
 
 
